20c9d7276db856221333bc3b92ffa7c99ea70cf4
FLEX <WebAchieve@github.com>
2021-07-17 12:28:07 +0300

    upload

---
+++ precache-manifest.4d5a6c5c12c62d37674ffa265c611c76.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffeb35ea4e351765609",
+    "url": "/TaskApp/css/app.d3127448.css"
+  },
+  {
+    "revision": "1b347223a9bbce0d295f",
+    "url": "/TaskApp/css/chunk-54e86da1.fb84ee6a.css"
+  },
+  {
+    "revision": "8ad15f301c7c8c374cd4",
+    "url": "/TaskApp/css/chunk-vendors.73e1f5b0.css"
+  },
+  {
+    "revision": "7f66642a4faacd3ac9512b36d5c1fda5",
+    "url": "/TaskApp/index.html"
+  },
+  {
+    "revision": "fffeb35ea4e351765609",
+    "url": "/TaskApp/js/app.f81288a4.js"
+  },
+  {
+    "revision": "96955588c144797adfac",
+    "url": "/TaskApp/js/chunk-2d0b64c8.17481e6e.js"
+  },
+  {
+    "revision": "ed758567181efdd25a6c",
+    "url": "/TaskApp/js/chunk-2d0cc274.315b7597.js"
+  },
+  {
+    "revision": "3c7cf6367a899f903a80",
+    "url": "/TaskApp/js/chunk-2d21ef2c.0b394767.js"
+  },
+  {
+    "revision": "1b347223a9bbce0d295f",
+    "url": "/TaskApp/js/chunk-54e86da1.5e5fd013.js"
+  },
+  {
+    "revision": "8ad15f301c7c8c374cd4",
+    "url": "/TaskApp/js/chunk-vendors.96d8794a.js"
+  },
+  {
+    "revision": "4245ec6ca3579bf22446d720cb0c0b1d",
+    "url": "/TaskApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TaskApp/robots.txt"
+  }
+]);
